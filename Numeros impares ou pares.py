6f3a2cd1dd6be914cdcaf10ea1dee3bef064992c
Nobre <juliolopesneto0@email.com>
2025-01-25 10:19:25 -0300

    Subindo o desafil 04

---
+++ Numeros impares ou pares.py
@@ -0,0 +1,8 @@
+print(' descubra se o numero é impar ou par ')
+print('')
+escrita = int(input('escreva um número = '))
+
+if escrita % 2 == 0:
+    print(f'{escrita} Esse número é par')
+else:
+    print(f'{escrita} Esse número é impar')
