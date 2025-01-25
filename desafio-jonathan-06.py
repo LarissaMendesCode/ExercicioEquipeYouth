320443ca1e51bd8563133b45831d78cfa87b014a
unknown <UP24107146@avponline.com.br>
2025-01-25 11:55:24 -0300

    Subindo o dasafio 06

---
+++ desafio-jonathan-06.py
@@ -0,0 +1,8 @@
+comprimento = float(input("Qual o comprimento do buraco: "))
+largura = float(input("Qual a largura do buraco: "))
+
+area = comprimento * largura
+
+calculo = area/2
+
+print(f"Serão necessarios {calculo} sacos de piche para tapar um buaraco de {area}m²")
